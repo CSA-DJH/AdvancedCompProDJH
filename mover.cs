ad84f6463fd6bfe819b3b48b3e5a1822465620c0
CSA-DJH <1865@chaparralstaracademy.com>
2016-12-12 13:13:44 -0600

    game code
    test

---
+++ mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour {
+
+
+	public float speed;
+	// Use this for initialization
+	void Update () {
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+	}
+
+
+}
